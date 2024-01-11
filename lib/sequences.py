abb47604cc985442a7bf75ed5b8da7cd3bcf9681
Boniface <cheruiyotbony@gmail.com>
2024-01-11 18:21:19 +0300

    done fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+   my_data=[]
+   for i in range(length):
+      if i == 0:
+         my_data.append(0)
+      elif i == 1:
+         my_data.append(1)
+      else:
+         next_number = my_data[-1] + my_data[-2]
+         my_data.append(next_number)
+         
+   print(my_data)
+   
+print_fibonacci(20)
